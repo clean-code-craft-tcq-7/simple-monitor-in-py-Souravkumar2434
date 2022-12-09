0d05973ead78bafd8743c24e993c3fdbdb4dd717
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-09 20:05:55 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -5,7 +5,7 @@ from check_charge_rate import check_charge_rate
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
-  #assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(50, 85, 0) is False)
   assert(check_soc(85) is False)
   assert(check_soc(25) is True)
   assert(check_temperature(40) is True)
