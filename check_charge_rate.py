106a6ed16f42cd4ddd8602653934e610e16d16ba
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-09 19:54:59 +0530

    Update check_charge_rate.py

--- check_charge_rate.py
+++ check_charge_rate.py
@@ -1,6 +1,7 @@
 
 def check_charge_rate(charge_rate):
+    charge_rate_check = True
     if charge_rate > 0.8:
-       return False
-    else:
-      return True
+       charge_rate_check = False
+    return charge_rate_check
+    
