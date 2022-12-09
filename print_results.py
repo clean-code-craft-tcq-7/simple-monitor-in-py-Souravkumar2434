b013ba4eb70225dea2c63930b6da187705e847dd
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-09 18:53:38 +0530

    Create print_results.py

---
+++ print_results.py
@@ -0,0 +1,9 @@
+
+def print_results(Temperature_check, soc_check, charge_rate_check):
+  if not (Temperature_check):
+    print('Temperature is out of range!')
+  if not (soc_check):
+    print('State of Charge is out of range!')
+  if not(charge_rate_check):
+    print('Charge rate is out of range!')
+    
