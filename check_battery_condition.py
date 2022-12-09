bd13f95b8b2034b2527fff428c8e64b76a44a623
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-09 19:24:36 +0530

    Update check_battery_condition.py

--- check_battery_condition.py
+++ check_battery_condition.py
@@ -1,12 +1,23 @@
 from check_soc import check_soc
 from check_temperature import check_temperature
 from check_charge_rate import check_charge_rate
-from print_results import print_results
+#from print_results import print_results
+
+
+def print_results(Temperature_check, soc_check, charge_rate_check):
+  if not (Temperature_check):
+    print('Temperature is out of range!')
+  if not (soc_check):
+    print('State of Charge is out of range!')
+  if not(charge_rate_check):
+    print('Charge rate is out of range!')
+    
+
 
 def battery_is_ok(temperature, soc, charge_rate):
   temperature_check = check_temperature(temperature)
   soc_check = check_soc(soc)
   charge_rate_check = check_charge_rate(charge_rate)
-  #print_results(temperature_check, soc_check, charge_rate_check)
+  print_results(temperature_check, soc_check, charge_rate_check)
   if(temperature_check and soc_check and charge_rate_check):
     return True
