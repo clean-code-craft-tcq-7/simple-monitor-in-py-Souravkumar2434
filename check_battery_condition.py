fea2977856a3d01f34ee73fc65cf7042f4fc4bfd
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-13 19:11:47 +0530

    Update check_battery_condition.py

--- check_battery_condition.py
+++ check_battery_condition.py
@@ -2,12 +2,15 @@ from check_soc import check_soc
 from check_temperature import check_temperature
 from check_charge_rate import check_charge_rate
 
+
+temperature_check = check_temperature(temperature)
+soc_check = check_soc(soc)
+charge_rate_check = check_charge_rate(charge_rate)
+
 def battery_is_ok(temperature, soc, charge_rate):
-  Battery_check = False
-  temperature_check = check_temperature(temperature)
-  soc_check = check_soc(soc)
-  charge_rate_check = check_charge_rate(charge_rate)
   if(temperature_check and soc_check and charge_rate_check):
-    Battery_check= True
-  return Battery_check
+    print("Battery is OK!")
+    return True
+  print("Battery is not OK!")
+  return False
 
