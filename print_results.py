25edc94e8a152ddce1000cd9fb8205334799f51e
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-13 19:03:14 +0530

    Delete print_results.py

--- print_results.py
+++
@@ -1,7 +0,0 @@
-def print_results(temperature_check, soc_check, charge_rate_check):
-  if not (temperature_check):
-    print('Temperature is out of range!')
-  if not (soc_check):
-    print('State of Charge is out of range!')
-  if not(charge_rate_check):
-    print('Charge rate is out of range!')
