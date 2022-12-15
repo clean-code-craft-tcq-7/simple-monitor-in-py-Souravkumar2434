910c89422291e57a5ddaa6f3a175b06eaaf2d8ea
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-15 18:07:24 +0530

    Update check_temperature.py

--- check_temperature.py
+++ check_temperature.py
@@ -1,6 +1,7 @@
-
+MINIMUM_THRESHOLD_FOR_TEMPERATURE = 0
+MAXIMUM_THRESHOLD_FOR_TEMPERATURE = 45
 def check_temperature(temperature):
-    if temperature < 0 or temperature > 45:
+    if temperature < MINIMUM_THRESHOLD_FOR_TEMPERATURE or temperature > MAXIMUM_THRESHOLD_FOR_TEMPERATURE:
         print("Temperature of battery is not OK!")
         return False
     print("Temperature of battery is OK!")
