9f393cd2dbf597b9717653ada6f9d2db6f1181ad
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-15 18:08:38 +0530

    Update check_soc.py

--- check_soc.py
+++ check_soc.py
@@ -1,6 +1,7 @@
-
+MINIMUM_THRESHOLD_FOR_SOC = 20
+MAXIMUM_THRESHOLD_FOR_SOC = 80
 def check_soc(soc):
-    if soc < 20 or soc > 80:
+    if soc < MINIMUM_THRESHOLD_FOR_SOC or soc > MAXIMUM_THRESHOLD_FOR_SOC:
         print("State of charge of battery is not OK!")
         return False
     print("State of charge of battery is OK!")
