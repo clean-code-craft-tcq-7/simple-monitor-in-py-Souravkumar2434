57715337c34227d98a9ca32f2953976c6c432d0b
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-12-15 18:09:36 +0530

    Update check_charge_rate.py

--- check_charge_rate.py
+++ check_charge_rate.py
@@ -1,6 +1,6 @@
-
+THRESHOLD_FOR_CHARGE_RATE = 0.8
 def check_charge_rate(charge_rate):
-    if charge_rate > 0.8:
+    if charge_rate > THRESHOLD_FOR_CHARGE_RATE:
         print("Charge rate of battery is not OK!")
         return False
     print("Charge rate of battery is OK!")
